167d58d46e9b54332958f820e818d7dcb8325e28
Yong Liu <yonmzn@amazon.com>
2022-09-07 16:39:26 +0800

    add env

--- nginx-fluent-bit/config/entrypoint.sh
+++ nginx-fluent-bit/config/entrypoint.sh
@@ -7,4 +7,6 @@ echo -n "set crontab job"
 
 echo -n "AWS for Fluent Bit Container Image Version "
 cat /AWS_FOR_FLUENT_BIT_VERSION
+
+sed -i "s/%%AWS_REGION%%/$AWS_REGION/g; s/%%AWS_S3_BUCKET%%/$AWS_S3_BUCKET/g;" /fluent-bit/etc/fluent-bit.conf
 exec /fluent-bit/bin/fluent-bit -e /fluent-bit/firehose.so -e /fluent-bit/cloudwatch.so -e /fluent-bit/kinesis.so -c /fluent-bit/etc/fluent-bit.conf
--- nginx-fluent-bit/send_test_data.sh
+++ nginx-fluent-bit/send_test_data.sh
@@ -1,5 +1,5 @@
 for i in `seq 1 100000`
 do 
  echo $i
- curl -XPOST http://localhost:8088/log -d "$i - this a test log from curl, this a test log from curl, this a test log from curl, this a test log from curl, this a test log from curl, this a test log from curl!"
+ curl -XPOST http://localhost:8088/log -d "$i - $(date -u "+%FT%T") this a test log from curl, this a test log from curl, this a test log from curl, this a test log from curl, this a test log from curl, this a test log from curl!"
 done 
