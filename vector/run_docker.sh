4ede672a91efd18cc99f5195aade1ca7b8bb1bb9
Yong Liu <yonmzn@amazon.com>
2022-09-16 17:30:42 +0800

    add vector

--- vector/run_docker.sh
+++ vector/run_docker.sh
@@ -2,10 +2,21 @@ docker build -t my-vector .
 
 dir_name=`pwd`
 
+#docker run -d  \
+#-p 8686:8686 \
+#-e INPUT_FILE=postdata-*.log \
+#-e AWS_REGION=ap-southeast-1 \
+#-e AWS_S3_BUCKET=nginx-fluent-bit-ap-southeast-1 \
+#-v $dir_name/data/vector:/var/lib/vector \
+#-v $dir_name/data/nginx:/var/log/nginx \
+#--network host \
+#my-vector
+
 docker run -d  \
--p 8686:8686 \
+-e INPUT_FILE=postdata-*.log \
 -e AWS_REGION=ap-southeast-1 \
 -e AWS_S3_BUCKET=nginx-fluent-bit-ap-southeast-1 \
 -v $dir_name/data/vector:/var/lib/vector \
 -v $dir_name/data/nginx:/var/log/nginx \
+--network host \
 my-vector
